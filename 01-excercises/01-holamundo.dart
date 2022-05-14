8c8e4f5fa789536fcd741adfaaf1aa3fb2e1cd43
ElfGod <albertogx@icloud.com>
2022-05-13 22:25:22 -0500

    Tipado de datos: Números

--- 01-excercises/01-holamundo.dart
+++ 01-excercises/01-holamundo.dart
@@ -1,3 +1,5 @@
+
+
 main(){
   print('Hola Mundo');
 }
---
+++ 01-excercises/02-datatypes.dart
@@ -0,0 +1,23 @@
+main() {
+  // ===== Numeros
+  var a = 10;
+  dynamic b = 20;
+  int c = 30;
+  double d = 40.0;
+
+  // int e = null; // Error
+  int? e = null;
+
+  int _f = 50;
+  double $g = 60.10;
+  
+  // int resultado = _f + $g; // Error
+
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+  print(_f);
+  print($g);
+}
