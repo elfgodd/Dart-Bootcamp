54116815cc52264475bc58787f5b509101eea4df
ElfGod <albertogx@icloud.com>
2022-05-13 23:52:21 -0500

    Tipado de datos: Mapas

--- 01-excercises/03-lists-sets-maps.dart
+++ 01-excercises/03-lists-sets-maps.dart
@@ -41,5 +41,51 @@ main() {
   var villiansList = heroes.toList();
   print(villiansList);
 
+
+  // ===== Maps // Dictionaries / Objects Literal
+  // Key, value pairs
+  var heroe = {
+    'name': 'ElfGodd',
+    'power': 'Green',
+    'age': 35,
+    'level': 9000
+  };
+  print(heroe);
+
+  // Map<int, dynamic> heroe2 = {}
+  Map<String, dynamic> heroe2 = {
+    'name': 'Batman',
+    'power': 'Money',
+    'age': 30,
+    'level': 4000
+  };
+
+  print(heroe2);
+  print(heroe['name']);
+  print(heroe['level']);
+
+  Map<double, dynamic> ironman = {
+    1.0: 'Tony Stark',
+    400: 'Intelligence and money',
+    2.6: 30,
+    7.77: 4000
+  };
+  print(ironman[1.0]);
+
+  // Another way to create a map
+  Map<String, dynamic> captain = new Map(); // Empty Map
+  // Error incompatible types: "int" cannot be assigned to "String"
+  captain.addAll(ironman) // Add all elements from ironman to captain
+
+  captain['name'] = 'Steve Rogers';
+  print(captain);
+
+  captain.addAll({
+    'power': 'Super strength',
+    'level': 65000
+  });
+
+  print(captain);
+
 }
 
