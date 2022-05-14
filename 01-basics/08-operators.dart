16ea6c39931eb3442890db8230ebc37582dea3f1
ElfGod <albertogx@icloud.com>
2022-05-14 18:44:16 -0500

    Operadores relacionales y por tipo (segunda parte)

--- 01-basics/08-operators.dart
+++ 01-basics/08-operators.dart
@@ -1,5 +1,5 @@
 main() {
-  // ===== operators
+  // ===== conditionals operators
 
   int a = 10;
   int? b;
@@ -26,4 +26,38 @@ main() {
   // int d = b2 ?? a;
   // print(d);  
 
+  // ===== relational operators
+  // ==, !=, >, <, >=, <=
+  // All return bool value
+  /*
+    > greater than
+    < less than
+    >= greater than or equal to
+    <= less than or equal to
+    == equal to
+    (!=) not equal to    
+  */
+
+  String person1 = 'Alberto';
+  String person2 = 'Josue';
+
+  print(person1 == person2);
+  print(person1 != person2);
+  
+  int x = 20;
+  int y = 30;
+  print(x > y); // false
+  print(x < y); // true
+  print(x >= y); // false
+  print(x <= y); // true
+
+  // Type operator, not that familiar
+  int i = 10;
+  String j = '10';
+
+  print(i is int); // true
+  print(j is int); // false
+
+  print(i is! int); // false
+  print(j is! int); // true
 }
