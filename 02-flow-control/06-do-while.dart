52107e6ee019674d524f5fba0c52c39d30f5d065
ElfGod <albertogx@icloud.com>
2022-05-14 23:02:10 -0500

    Ciclo Do While

--- 02-flow-control/06-do-while.dart
+++ 02-flow-control/06-do-while.dart
@@ -2,16 +2,21 @@
 // @dart=2.9
 import 'dart:io';
 main() {
-  // ===== while
+  // ===== do while
+
 
   String continueVar = 'y';
   int counter = 0;
 
-  while(continueVar == 'y') {
+  // do while always runs at least once
+  // while the code may not run at all
+  do {
+
     counter ++;
     stdout.writeln('Counter: $counter');
 
     stdout.writeln('want to continue? (Y/n)');
     continueVar = stdin.readLineSync() ?? 'n';
-  }
+
+  } while(continueVar == 'y');
 }
