5f8af292031df857c2dafc8147efebb1b1c4e7b8
ElfGod <albertogx@icloud.com>
2022-05-14 20:14:36 -0500

    For Loop, Solución de la tabla de multiplicar con un ciclo For

---
+++ 02-flow-control/03-for.dart
@@ -0,0 +1,18 @@
+
+// @dart=2.9
+import 'dart:io';
+main() {
+  for(int i = 0 ; i < 10; i++) {
+    print('index i: $i');
+    print('indeXYZ i + i: ${i + i}');
+  }
+
+  stdout.writeln('Enter Mathematical base table number');
+  int base = int.parse(stdin.readLineSync() ?? '5');
+
+  for (var i = 0; i < 10; i++) {
+    stdout.writeln('$base x $i = ${base * i}');
+  }
+
+
+}
