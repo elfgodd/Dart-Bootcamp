e9c30c6d15ae0fa171dc009d459f88fa3c1414ac
ElfGod <albertogx@icloud.com>
2022-05-15 19:26:19 -0500

    Streams - onError, onDone y cancelOnError

--- 04-uncommon-types/07-streams.dart
+++ 04-uncommon-types/07-streams.dart
@@ -5,11 +5,29 @@ void main() {
   // StreamController<dynamic>
   final streamController = StreamController();
 
-  streamController.stream.listen((data) {
-    print('taking off! $data');
-  });
+  streamController.stream.listen(
+    (data) => print('taking off! $data'),
+    onError: (error) => print('error: $error'),
+    // This will cancel the stream, if an error occurs.
+    onDone: () => print('mission completed'),
+    // onDone will kick in after the stream is closed.
+    // We can add like a success message anythig we want.
+    cancelOnError: false
+  );
 
   streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.addError('Houston, we have a problem!');
+  streamController.sink.add('Apollo 14');
+  streamController.sink.add('Apollo 15');
+
+  // We should cancel the stream
+  // when we know we don't need it anymore.
+  streamController.close();
+
+  // Cannot add event after closing
+  // streamController.sink.add('Apollo 16');
 
   print('end of main');
 }
