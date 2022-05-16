2da5a2d5b3ab767eae906b05e674107f89618abb
ElfGod <albertogx@icloud.com>
2022-05-15 20:06:38 -0500

    Clases en archivos independientes

---
+++ 05-classes/02-class2.dart
@@ -0,0 +1,11 @@
+import 'classes/person.dart';
+main() {
+
+  final person = new Person();
+  person..name = 'Alberto'
+        ..age = 35
+        ..bio = 'Was born in Colombia';
+
+  print(person);
+}
+
---
+++ 05-classes/classes/person.dart
@@ -0,0 +1,15 @@
+class Person {
+  // Properties
+  String? name;
+  int? age;
+  String? bio;
+
+  // Gets and Sets
+
+  // Constructors
+
+  // Methods
+  @override
+  String toString() => '$name $age $bio';
+
+}
