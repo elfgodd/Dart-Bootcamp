b65cb733160582cc20fb56a010b15c7e78c0e21a
ElfGod <albertogx@icloud.com>
2022-05-17 21:48:34 -0500

    Override

--- 06-classes-advanced/04-override.dart
+++ 06-classes-advanced/04-override.dart
@@ -13,6 +13,18 @@ class Client extends Person {
 
   Client(int ageNow, String nameNow) : 
   super(nameNow, ageNow); // super constructor
+  // When we override a method from a father class
+  // It is recommended we call the @override annotation
+  @override
+  void printInfo() => {
+    // This super.printInfo() will call the father class method
+    // first and then the child class method
+    // Name: Pedro, Age: 35
+    // Client: Pedro, Age: (35)
+
+    super.printInfo(),
+    print('Client: $name, Age: ($age)')
+  };
 }
 
 void main(List<String> args) {
