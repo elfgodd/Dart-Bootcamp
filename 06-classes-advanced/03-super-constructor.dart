e30d46aa809a48aaab964ec956707b754d8bf97a
ElfGod <albertogx@icloud.com>
2022-05-17 21:38:45 -0500

    Super constructor

---
+++ 06-classes-advanced/03-super-constructor.dart
@@ -0,0 +1,26 @@
+class Person {
+  String? name;
+  int? age;
+
+  Person(this.name, this.age);
+
+  void printInfo() => {print('Name: $name, Age: $age')};
+}
+
+class Client extends Person {
+  String? address;
+  List orders = [];
+
+  Client(int ageNow, String nameNow) : 
+  super(nameNow, ageNow); // super constructor
+  // Client(String name, int age, this.company) : super(name, age);
+  // @override
+  // void printInfo() => {print('Name: $name, Age: $age, Company: $company')};
+}
+
+void main(List<String> args) {
+  final pedro = new Client(35, 'Pedro');
+  pedro.printInfo();
+  // pedro.orders;
+  // pedro.address;
+}
